353e3f892af79c6827e57f54ef63fb918fcb92b7
amaurygl <amaury.gelin@hotmail.fr>
2019-10-02 08:13:11 +0200

    end of td1

--- prep_bulletin.pl
+++ prep_bulletin.pl
@@ -1,16 +1,22 @@
 #!/usr/bin/env perl
 
-#fournir des preuves qu'on a bien choppé toutes les balises
-#convertir le texte avec entités html en texte avec la fonction convert.pl fournie
-#pour le texte, virer toutes les balises à l'intérieur + extraire les images
-    #recupérer tout le bloc de texte
-    #boucler sur le texte pour enlever chaque image
+# This script goes through a file of from the corpus "BULLETINS" and creates an XML that gathers the information of the file in a structure that will be easily indexable
+# The regular expressions that let us find elements in the file are proved to be sound (see commandes_unix.txt)
 
-open($fd,'<','OLD_BULLETINS_LO17/75458.htm') or die("open: $!");
-#open($fd,'<',$ARGV[1]) or die("open: $!");
+sub remove_html_tags {
+    $html_text = $_[0]; # passing argument
+    ($plain_text = $html_text) =~ s/<[^>]*>//gs;
+    return $plain_text;
+}
 
-$flag_image = 0;
-while(<$fd>) {
+open($fd,'<',$ARGV[0]) or die("open: $!"); # open the file passed an an argument in READ mode 
+
+$flag_image = 0; # flag to deal with images that don't have a legend
+$flag_multiline_text = 0;
+$texte = '';
+
+# loop over each line of the file
+while(<$fd>) { 
     if($_ =~ /<span class="style95" style="color:inherit">(\d+)/) {
         $fichier = $1;
     }
@@ -26,20 +32,32 @@ while(<$fd>) {
     if($_ =~ /<span class="style17">(.*)<\/span>/) {
         $titre = $1;
     }
-    if($_ =~ /<p class="style96"><span class="style95">(.*)<\/span>/) {
+
+    if($_ =~ /<p class="style96"><span class="style95">(.*)<\/span>/) { # full text on one line
         $texte = $1;
     }
+
+    if($_ =~ /<p class="style96"><span class="style95">(.*)<br \/>/) { # multi-line text
+        $flag_multiline_text = 1;
+        $texte = $1;
+    }
+    if($flag_multiline_text && $_ =~ /<\/span>/) {
+        $flag_multiline_text = 0; # end of multi-line text
+    }
+    if($flag_multiline_text && $_ !=~ /\\n/) {
+        $texte = $texte.$_; # add full line to the text
+    }
+
     if($_ =~ /<div style="text-align: center"><img src="(.*\.\w{3,4})/){
-        $flag_image = 1 ;
-        $url_image = $1 ;
-        #$images{$1} = $2;
+        $flag_image = 1;
+        $url_image = $1;
     }
-    if ($flag_image && $_ =~ /<span class="style21"><strong>(.*)<\/strong>/){
+    if($flag_image && $_ =~ /<span class="style21"><strong>(.*)<\/strong>/){
         $images{$url_image} = $1;
-        #print("legende: $1");
-        $flag_image = 0 ;
+        $flag_image = 0;
     }
-    if ($_ =~ /<p class="style44"><span class="style85">(.*)<\/span>/){
+
+    if($_ =~ /<p class="style44"><span class="style85">(.*)<\/span>/){
         $contact = $1;
     }
 }
@@ -49,13 +67,17 @@ print("<numero>$numero</numero>\n");
 print("<date>$date</date>\n");
 print("<rubrique>$rubrique</rubrique>\n");
 print("<titre>$titre</titre>\n");
-#print("<texte>$texte<texte>\n");
-while (($url, $legende) = each(%images)) {
+$texte = remove_html_tags($texte);
+print("<texte>$texte</texte>\n");
+while(($url, $legende) = each(%images)) {
     $balisesImages = $balisesImages."<image><urlImage>$url</urlImage><legendeImage>$legende</legendeImage></image>";
 }
-print("<images>$balisesImages</images>\n");
+$size = length $balisesImages;
+if($size > 0) { # if there is no images, we don't display the tag <images>
+    print("<images>$balisesImages</images>\n");
+
+}
 print("<contact>$contact</contact>\n");
 print("</bulletin>\n");
-
 
 close($fd);
--- prep_corpus.pl
+++ prep_corpus.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/env perl
 
+# This script goes through all the files of from the corpus "BULLETINS" and call prep_bulletin.pl for each of them
+# ./prep_corpus > SomeFile.xml (to create an XML file that contains the ouput) 
+# ./convert.pl test.xml > FinalFile.xml (to remove the HTML special characters of the output)
+
 $Path = "OLD_BULLETINS_LO17";
 
 opendir(my $rep, $Path) or die "Impossible d'ouvrir le repertoire $Path\n";
-#my @files = grep { !/^\.\.?$/ } readdir($rep);
 my @files = grep { $_ =~ /\d{5}/ } readdir($rep);
 closedir ($rep);
 
@@ -11,8 +14,8 @@ print("<corpus>\n");
 
 foreach my $file(@files) {
     if ( -f "$Path/$file") {
-        #`prep_bulletin.pl $file`;
-        `prep_bulletin.pl`;
+        $filePath = "$Path/$file";
+        system($^X, "prep_bulletin.pl", ($filePath));
     }
 }
 
