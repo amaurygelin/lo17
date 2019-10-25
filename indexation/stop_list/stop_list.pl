a9ad524ffd0c3427ce6eb0a1e388049261223bc6
amaurygl <amaury.gelin@hotmail.fr>
2019-10-25 12:45:26 +0200

    last

--- indexation/stop_list/stop_list.pl
+++ indexation/stop_list/stop_list.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/env perl
 
-open($fd,'<','./df_perc.txt') or die("open: $!");
+open($fd,'<','../statistics/df_perc.txt') or die("open: $!");
 
 while(<$fd>) {
     if($_ =~ /(.*)\t(.*)/) {
         if($2 >= 50) { 
+            #TODO: remplacer le 50% de seuil en regardant à l'oeil le premier mot qui a l'air interessant dans la liste décroissante de df
             # if the word appears in at least 50% of the documents, add it to the stop list
             print("$1\n");
         }
