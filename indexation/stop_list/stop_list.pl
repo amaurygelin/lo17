c5865bd27ca802712732dccb1a85ec20e38ad6a9
amaurygl <amaury.gelin@hotmail.fr>
2019-10-27 18:42:36 +0100

    last

--- indexation/stop_list/stop_list.pl
+++ indexation/stop_list/stop_list.pl
@@ -4,13 +4,14 @@ open($fd,'<','../statistics/df_perc.txt') or die("open: $!");
 
 use Scalar::Util qw(looks_like_number);
 
+# The threshould 35.4 has been tuned manually
+# We looked at sorted df_percentage and noticed that the first valuable word is before 35.4%
+# We don't consider "recherche" and "chercheurs" as valuable words because all articles are about research. 
+# We also put numbers into the stop list.
+
 while(<$fd>) {
     if($_ =~ /(.*)\t(.*)/) {
         if(looks_like_number($1) || $2 >= 35.4) { 
-            # The threshould 35.4 has been tuned manually
-            # We looked at sorted df_percentage and noticed that the first valuable word is before 35.4%
-            # We don't consider "recherche" and "chercheurs" as valuable words because all articles are about research. 
-            # We also put numbers into the stop list.
             print("$1\n");
         }
     }
